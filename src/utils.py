b05aed570b46bfe8f960461769f68c1f9aa59ee9
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-04 12:22:29 +0300

    add sort_by_executed_and_date function to utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,10 @@ def open_json(path):
         file_json = file.read()
     operations = json.loads(file_json)
     return operations
+
+
+def sort_by_executed_and_date(array: list) -> dict:
+    """Функция получает на вход список словарей.
+    Возвращает список словарей с последними пятью
+    выполнеными операциями"""
+    pass
