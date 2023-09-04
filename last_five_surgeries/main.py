a862c8c85ed8060bc67cc5ff747f0ce16a1c3460
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-04 13:08:15 +0300

    created main function in main.py

---
+++ last_five_surgeries/main.py
@@ -0,0 +1,25 @@
+from last_five_surgeries.setting import PATH
+from src import utils
+
+
+def main():
+    operations = utils.open_json(PATH)
+    last_5_operations = utils.sort_by_executed_and_date(operations)
+    for item in last_5_operations:
+        date_operation = utils.get_information(item, 'date')
+        operation_descript = utils.get_information(item, "description")
+        information_from = utils.get_information(item, 'from')
+        information_to = utils.get_information(item, 'to')
+        operation_amount = utils.get_information(item, 'operationAmount')
+        print(utils.get_report(
+            date_operation,
+            operation_descript,
+            information_from,
+            information_to,
+            operation_amount)
+        )
+        print()
+
+
+if __name__ == '__main__':
+    main()
