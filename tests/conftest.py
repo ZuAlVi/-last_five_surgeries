32d93f8e22dd410d3f55be19737e8f7569e35edc
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-05 13:55:02 +0300

    add array_fixture_2 function in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,18 @@ ARRAY = [
     {"state": "EXECUTED", "date": "2019-09-11T17:30:34.445824"}
 ]
 
+ARRAY_2 = [
+    {"state": "EXECUTED", "date": "2019-08-26T10:50:58.294041"},
+    {"state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+]
+
 
 @pytest.fixture
 def array_fixture():
     return ARRAY.copy()
+
+
+@pytest.fixture
+def array_fixture_2():
+    return ARRAY_2.copy()
