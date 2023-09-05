c331ea29fee187412f4eeb636567cee5ffbca16b
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-05 14:08:13 +0300

    Delete open_json function from utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,3 @@
-import json
-
-
-def open_json(path):
-    """Функция получает на вход json файл.
-    Возвращает список словарей с данными."""
-    with open(path, "r", encoding='utf-8') as file:
-        file_json = file.read()
-    operations = json.loads(file_json)
-    return operations
-
-
 def sort_by_executed(array: list) -> list:
     """Функция получает на вход список словарей.
     Возвращает список словарей отсортрованных по
